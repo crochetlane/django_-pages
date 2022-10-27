7240d8613c848bdefb8ab860db93a60f05ea446e
crochetlane <crochet.lane@gmail.com>
2022-10-27 11:53:00 -0500

    configuring for heroku

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-zcstt+!1+3e*-&zg$n(_&62^)l1(vq)aw7!_tg247w85!g)9^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
